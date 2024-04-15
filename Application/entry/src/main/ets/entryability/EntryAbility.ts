1c7d2558a5629f02b6d8b4879764a4eac711a7e8
1693905917 <1693905917@qq.com>
2024-04-15 15:09:57 +0800

    云函数细节

--- Application/entry/src/main/ets/entryability/EntryAbility.ts
+++ Application/entry/src/main/ets/entryability/EntryAbility.ts
@@ -2,11 +2,20 @@ import hilog from '@ohos.hilog';
 import UIAbility from '@ohos.app.ability.UIAbility';
 import Window from '@ohos.window';
 import abilityAccessCtrl from '@ohos.abilityAccessCtrl';
+//第一步 引入initialize函数  从 oh-package.json5文件中的引入 "@hw-agconnect/hmcore": "^1.0.0",
 import { initialize } from '@hw-agconnect/hmcore';
 import buffer from '@ohos.buffer';
 
+// @ts-ignore
+import json from  '../../resources/rawfile/agconnect-services.json'
+
 export default class EntryAbility extends UIAbility {
   onCreate() {
+    try {
+      initialize(this.context, json);
+    } catch (e) {
+      hilog.error(0x0000,'AGConnectError',JSON.stringify(e))
+    }
     let AtManager = abilityAccessCtrl.createAtManager();
     AtManager.requestPermissionsFromUser(this.context, ['ohos.permission.READ_MEDIA', 'ohos.permission.MEDIA_LOCATION']).then((data) => {
       hilog.info(0x0000, 'testTag', '%{public}s', 'request permissions from user success' + data);
@@ -21,13 +30,16 @@ export default class EntryAbility extends UIAbility {
   }
 
   async onWindowStageCreate(windowStage: Window.WindowStage) {
-    const context = this.context
-    const value = await context.resourceManager.getRawFileContent("agconnect-services.json");
-    let json: string = buffer.from(value).toString("utf8");
-    initialize(this.context, JSON.parse(json));
+    // const context = this.context
+    // //第一步：利用resourceManager.getRawFileContent方法去await异步读取了agconnect-services.json文件，而读取到的结果类型是Uint8Array：二进制数组类型
+    // const value = await context.resourceManager.getRawFileContent("agconnect-services.json");
+    // //将结果的二进制数组类型转换成字符串类型
+    // let json: string = buffer.from(value).toString("utf8");
+    //this.context:传递的是UIAbility的上下文对象  json是从agconnect-services.json文件中传递进来的，所以上三行代码就是为了解析agconnect-services.json文件的配置文件信息
+    //initialize(this.context, json);
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-    windowStage.loadContent('pages/Index', (err, data) => {
+    windowStage.loadContent('pages/MyIndex', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
--- CloudProgram/cloudfunctions/hello/hello.ts
+++ CloudProgram/cloudfunctions/hello/hello.ts
@@ -3,10 +3,17 @@ let myHandler = async function (event, context, callback, logger) {
 
   // do something here
 
+  const obj=JSON.parse(event.body)
+  const name=obj.name
+  //context存储的数据有环境变量,而context它有一个叫env(environment)对象,env就包含了当前云函数在运行时的所有的环境变量，包括它系统提供的环境变量和我们将来可以自定义的环境变量
+  //假设我们自定义的环境变量叫age
+  const age=context.env.age
+
   callback({
     code: 200,
-    desc: "Hello Cloud Function."
+    message: `Hello ${name},age=${age}`
   });
 };
 
 export { myHandler };
+
