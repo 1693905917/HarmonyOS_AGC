701b84571cb15cf72d485dad6f62966c51f964ee
1693905917 <1693905917@qq.com>
2024-04-16 23:19:55 +0800

    认证服务-使用认证组件进行认证

--- Application/entry/src/main/ets/entryability/EntryAbility.ts
+++ Application/entry/src/main/ets/entryability/EntryAbility.ts
@@ -39,7 +39,7 @@ export default class EntryAbility extends UIAbility {
     //initialize(this.context, json);
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-    windowStage.loadContent('pages/MyIndex', (err, data) => {
+    windowStage.loadContent('pages/MyLogin', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
